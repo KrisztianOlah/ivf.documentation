3200e1d83fabae3e19aed29bb055728f5e31cd16
Jozef Chmelar <jozef.chmelar.ml@mts.sk>
2020-12-17 11:22:11 +0100

    Generating history

--- build.ps1
+++ build.ps1
@@ -1,5 +1,30 @@
-rm -r _site/ -erroraction silentlycontinue 
+# Get the latest tag from IVF
+Push-Location ..\ivf
+$version = (git describe)+"-debug2"
+Pop-Location
+
+# Clean build
 rm -r bin/ -erroraction silentlycontinue 
 rm -r obj/ -erroraction silentlycontinue 
+rm -r docs/ -erroraction silentlycontinue 
+
+# Build docu
 dotnet build -v q
-Start-Process .\_site\index.html
+
+# Create a history docu
+mkdir .\history\$version -ErrorAction SilentlyContinue 
+Copy-Item .\docs\* .\history\$version -Recurse -Force 
+
+# Create history linkes
+## Get the historical versions and create links.
+$historyItems = (Get-ChildItem .\history -n -dir)
+
+$historyMD = "# Previous versions `n"
+foreach ($item in $historyItems){
+    $historyMD+="[$item](../../history/$item/index.html)`n`n" 
+}
+
+# Write the to a file.
+$historyMD > .\previous_versions\index.md
+
+# Start-Process .\_site\index.html
